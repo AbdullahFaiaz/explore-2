d788262d637e34bf0a982351ac29d4699fdfdb4c
AbdullahFaiaz <talha0184999@gmail.com>
2024-03-10 21:48:55 +0600

    trying to fix missing props issue

--- src/ShowUser.jsx
+++ src/ShowUser.jsx
@@ -1,12 +1,36 @@
-import './ShowUser.css'
-export default function ShowUser({info}){
-    const {id,name,email} = info
-    return (
-        <div className="design">
-        <h4>Id: {id}</h4>
-        <h4>Name:{name}</h4>
-        <h4>Email: {email}</h4>
-        </div>
+// import './ShowUser.css'
+// export default function ShowUser({info}){
+//     const {id,name,email} = info
+//     return (
+//         <div className="design">
+//         <h4>Id: {id}</h4>
+//         <h4>Name:{name}</h4>
+//         <h4>Email: {email}</h4>
+//         </div>
 
-    )
+//     )
+// }
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ShowUser.css';
+
+export default function ShowUser({ info }) {
+  const { id, name, email } = info;
+
+  return (
+    <div className="design">
+      <h4>Id: {id}</h4>
+      <h4>Name: {name}</h4>
+      <h4>Email: {email}</h4>
+    </div>
+  );
 }
+
+ShowUser.propTypes = {
+  info: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+  }).isRequired,
+};
